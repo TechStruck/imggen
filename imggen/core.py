b35fa2cc2f88b4e1b73d2d53b2e981bec3fab7ec
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-04-11 20:57:04 +0530

    Added utils for textdraw

--- imggen/core.py
+++ imggen/core.py
@@ -3,30 +3,53 @@ import functools
 import io
 import os
 import pathlib
+import threading
 from collections.abc import MutableMapping
 from typing import Any, Callable, Dict, Optional, Tuple, Union
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 ImageCacheDict = Dict[str, Image.Image]
+FontCacheDict = Dict[str, bytes]
 ImageType = Union[Image.Image, bytes, io.BytesIO, str]
 
+PathType = Union[str, pathlib.Path]
 
-class ImageCache(MutableMapping):
-    def __init__(
-        self, *, cache_dict: ImageCacheDict, basepath: Union[str, pathlib.Path]
-    ):
+
+class AssetCache(MutableMapping):
+    def __init__(self, *, cache_dict: Dict, basepath: PathType):
         self.cache_dict = cache_dict
+        self.lock = threading.Lock()
 
         if isinstance(basepath, pathlib.Path):
             self.basepath = basepath
         else:
             self.basepath = pathlib.Path(basepath)
 
+    def __setitem__(self, key: str, value) -> None:
+        with self.lock:
+            self.cache_dict[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        with self.lock:
+            del self.cache_dict[key]
+
+    def __iter__(self):
+        return self.cache_dict.__iter__()
+
+    def __len__(self):
+        return len(self.cache_dict)
+
+
+class ImageCache(AssetCache):
+    def __init__(self, *, cache_dict: ImageCacheDict, basepath: PathType):
+        super().__init__(cache_dict=cache_dict, basepath=basepath)
+
     def __getitem__(self, key: str) -> Image.Image:
-        # In cache
-        if key in self.cache_dict:
-            return self.cache_dict[key]
+        with self.lock:
+            # In cache
+            if key in self.cache_dict:
+                return self.cache_dict[key]
 
         # Load and populate cache
         img = Image.open(self.basepath / key)
@@ -34,17 +57,27 @@ class ImageCache(MutableMapping):
 
         return img
 
-    def __setitem__(self, key: str, value: Image.Image) -> None:
-        self.cache_dict[key] = value
 
-    def __delitem__(self, key: str) -> None:
-        del self.cache_dict[key]
+class FontCache(AssetCache):
+    def __init__(self, *, cache_dict: FontCacheDict, basepath: PathType):
+        super().__init__(cache_dict=cache_dict, basepath=basepath)
 
-    def __iter__(self):
-        return self.cache_dict.__iter__()
+    def __getitem__(self, key: Tuple[str, int]) -> ImageFont.FreeTypeFont:
+        filename, size = key
 
-    def __len__(self):
-        return len(self.cache_dict)
+        with self.lock:
+            if filename in self.cache_dict:
+                return ImageFont.truetype(
+                    io.BytesIO(self.cache_dict[filename]), size=size
+                )
+
+        with open(self.basepath / filename, "rb") as f:
+            fontbytes = f.read()
+
+        font = ImageFont.truetype(io.BytesIO(fontbytes), size=size)
+        self.cache_dict[filename] = fontbytes
+
+        return font
 
 
 def generator(func: Callable):
@@ -80,14 +113,39 @@ class BaseImageGenerator:
         *,
         async_mode: bool = False,
         image_cache: ImageCacheDict = {},
-        basepath: Union[str, pathlib.Path],
+        font_cache: FontCacheDict = {},
+        image_basepath: Union[str, pathlib.Path],
+        font_basepath: Union[str, pathlib.Path],
         loop: Optional[asyncio.BaseEventLoop] = None
     ):
         self.async_mode = async_mode
-        self.image_cache = ImageCache(cache_dict=image_cache.copy(), basepath=basepath)
+        self.image_cache = ImageCache(
+            cache_dict=image_cache.copy(), basepath=image_basepath
+        )
+        self.font_cache = FontCache(
+            cache_dict=font_cache.copy(), basepath=font_basepath
+        )
         self.loop = loop
         if async_mode and loop is None:
             self.loop = asyncio.get_event_loop()
+
+    def convert_to_image(self, image: ImageType) -> Image.Image:
+        # Conversions
+        if isinstance(image, str):
+            image = Image.open(image)
+        if isinstance(image, bytes):
+            image = io.BytesIO(image)
+        if isinstance(image, io.BytesIO):
+            image = Image.open(image)
+        if not isinstance(image, Image.Image):
+            raise TypeError("Invalid type received for image")
+        return image
+
+    def image_to_bytesio(self, image: Image.Image, format: str = "JPEG"):
+        b = io.BytesIO()
+        image.save(b, format=format)
+        b.seek(0)
+        return b
 
     def paste(
         self,
@@ -99,21 +157,31 @@ class BaseImageGenerator:
         resize: Tuple[int, int] = None
     ):
         base = self.image_cache[basename]
-        # Conversions
-        if isinstance(image, str):
-            image = Image.open(image)
-        if isinstance(image, bytes):
-            image = io.BytesIO(image)
-        if isinstance(image, io.BytesIO):
-            image = Image.open(image)
-        if not isinstance(image, Image.Image):
-            raise TypeError("Invalid type received for image")
+        image = self.convert_to_image(image)
 
         if resize:
             image = image.resize(resize)
         base.paste(image, coords)
 
-        b = io.BytesIO()
-        base.save(b, format=format)
-        b.seek(0)
-        return b
+        return self.image_to_bytesio(base, format=format)
+
+    def writetext(
+        self,
+        imagename: str,
+        *,
+        fontname: str,
+        size: int,
+        center: Tuple[int, int],
+        text: str,
+        fill: Tuple[int, int, int] = (0, 0, 0),
+        format: str = "JPEG"
+    ):
+        image = self.image_cache[imagename]
+
+        font = self.font_cache[fontname, size]
+        xy = (center[0] - font.getlength(text) // 2, center[1] - size // 2)
+
+        draw = ImageDraw.Draw(image)
+        draw.text(xy, text, fill=fill, font=font)
+
+        return self.image_to_bytesio(image, format=format)
--- imggen/meme.py
+++ imggen/meme.py
@@ -1,18 +1,44 @@
 import os
 import pathlib
 
-from .core import BaseImageGenerator, ImageCacheDict, ImageType, generator
+from .core import (
+    BaseImageGenerator,
+    FontCacheDict,
+    ImageCacheDict,
+    ImageType,
+    generator,
+)
 
 
 class MemeGenerator(BaseImageGenerator):
-    def __init__(self, async_mode: bool = False, image_cache: ImageCacheDict = {}):
-        path = pathlib.Path(os.path.abspath(__file__)).parent / "images/meme/"
+    def __init__(
+        self,
+        async_mode: bool = False,
+        image_cache: ImageCacheDict = {},
+        font_cache: FontCacheDict = {},
+    ):
+        image_path = pathlib.Path(os.path.abspath(__file__)).parent / "images/meme/"
+        font_path = pathlib.Path(os.path.abspath(__file__)).parent / "fonts/"
+
         super().__init__(
-            basepath=path,
+            image_basepath=image_path,
+            font_basepath=font_path,
             image_cache=image_cache,
+            font_cache=font_cache,
             async_mode=async_mode,
         )
 
     @generator
     def wanted(self, pfpdata: ImageType):
         return self.paste("wanted.jpg", pfpdata, (269, 451), resize=(395, 395))
+
+    @generator
+    def worthless(self, text: str):
+        return self.writetext(
+            "worthless.jpg",
+            fontname="OpenSans-Light.ttf",
+            size=100,
+            center=(678, 423),
+            text=text,
+        )
+        # return self.writetext("worthless.jpg", fontname="ani.ttf", size=100, center=( 423, 678), text=text)
